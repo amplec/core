66a058fd1f3e9d54391e2d88a0ef15119feb69f8
mo <~>
2025-01-14 18:11:05 +0100

    feat: added support for full sized triage resulsts, configs, and improved conciseveness of the output in general

--- src/preprocessing/karton_preprocessor.py
+++ src/preprocessing/karton_preprocessor.py
@@ -1,7 +1,7 @@
 from utils.logger import Logger
 import requests, os
 from dotenv import load_dotenv
-from typing import List, override
+from typing import List, override, Optional
 from .preprocessor import Preprocessor
 
 class KartonPreprocessor(Preprocessor):
@@ -32,9 +32,14 @@ class KartonPreprocessor(Preprocessor):
         :return: processed data
         """
         
+        self.triage_id_list = []
+        self.configs = {}
+        
         ret_dict = {
             "hierarchy": {},
-            "results": {}
+            "results": {},
+            "configs": {},
+            "triage_results": {}
         }
         
         payload_results = data.get("payload_results")
@@ -50,6 +55,14 @@ class KartonPreprocessor(Preprocessor):
             return {}
         self.log.info(f"Now processing {len(payloads)} payloads")
         ret_dict["hierarchy"] = self._build_hierarchy_from_payload_dict(payloads)
+        
+        self.log.info(f"Now downloading {len(self.triage_id_list)} triage results")
+        for id in self.triage_id_list:
+            ret_dict["triage_results"][id] = self._retrieve_triage_data(id, "report")
+            
+        self.log.info(f"Now processing {len(self.configs)} configs")
+        for sha256, config in self.configs.items():
+            ret_dict["configs"][sha256] = self._process_config_payload(config)
         
         return ret_dict
          
@@ -112,7 +125,8 @@ class KartonPreprocessor(Preprocessor):
                 # Extract submission id
                 submission_id = result_entry.get("data", {}).get("submission_id", "")
                 if submission_id:
-                    triage_overview = self._retrieve_triage_overview(submission_id)
+                    self.triage_id_list.append(submission_id)
+                    triage_overview = self._retrieve_triage_data(submission_id)
                     return_entry["data"] = triage_overview
                 else:
                     self.log.error(f"Failed to extract Submission ID from a detected triage result entry! payload_id: {result_entry.get('payload_id', 'N/A')}")
@@ -124,25 +138,34 @@ class KartonPreprocessor(Preprocessor):
             
                 
                 
-    def _retrieve_triage_overview(self, triage_id:str) -> dict:
+    def _retrieve_triage_data(self, triage_id:str, triage_type:Optional[str]="overview" ) -> dict:
         """
-        Retrieves the triage overview for a given triage id
+        Retrieves the triage data for a given triage id
         
-        :param triage_id: triage id for which the overview should be retrieved
+        :param triage_id: triage id for which the data should be retrieved
         :type triage_id: str
-        :return: triage overview
+        :param triage_type: type of triage data to be retrieved, defaults to the triage overview. Possible values are "overview" and "report"
+        :type triage_type: Optional[str], optional
+        :return: triage data
         """
         
         return_dict = {}
         
-        self.log.info(f"Retrieving triage overview for triage id: {triage_id}")
+        if triage_type not in ["overview", "report"]:
+            self.log.error(f"Invalid triage type: {triage_type}")
+            return return_dict
         
-        #response = requests.get(url=f"{self.triage_url}samples/{triage_id}/behavioral1/report_triage.json", headers={"Authorization": f"Bearer {self.api_key}"})
+        self.log.info(f"Retrieving triage {triage_type} for triage id: {triage_id}")
         
-        response = requests.get(url=f"{self.triage_url}samples/{triage_id}/overview.json", headers={"Authorization": f"Bearer {self.api_key}"})
+        if triage_type == "report":
+            url = f"{self.triage_url}samples/{triage_id}/behavioral1/report_triage.json"
+        else:
+            url = f"{self.triage_url}samples/{triage_id}/overview.json"
+        
+        response = requests.get(url=url, headers={"Authorization": f"Bearer {self.api_key}"})
         
         if response.status_code != 200:
-            self.log.error(f"Failed to retrieve triage overview for triage id: {triage_id}")
+            self.log.error(f"Failed to retrieve triage {triage_type} for triage id: {triage_id}")
             self.log.error(f"Response: {response.text}")
         else:
             return_dict["analysis"] = response.json().get("analysis", {})
@@ -151,32 +174,58 @@ class KartonPreprocessor(Preprocessor):
             return_dict["sha256"] = sample.get("sha256", "")
             return_dict["timestamp"] = sample.get("completed", "")
             return_dict["id"] = sample.get("id", "")
-            
-            return_dict["signatures"] = response.json().get("signatures", [])
-            
-            targets = response.json().get("targets", [])
-            return_dict["iocs"] = {
-                "ips": [],
-                "urls": [],
-                "domains": [],
-                "emails": [],
-            }
-            for target in targets:
-                iocs = target.get("iocs", {})
-                for domain in iocs.get("domains", []):
-                    if domain not in return_dict["iocs"]["domains"] and not "in-addr.arpa" in domain:
-                        return_dict["iocs"]["domains"].append(domain) 
-                for ip in iocs.get("ips", []):
-                    if ip not in return_dict["iocs"]["ips"]:
-                        return_dict["iocs"]["ips"].append(ip)
-                for url in iocs.get("urls", []):
-                    if url not in return_dict["iocs"]["urls"]:
-                        return_dict["iocs"]["urls"].append(url)
-                for email in iocs.get("emails", []):
-                    if email not in return_dict["iocs"]["emails"]:
-                        return_dict["iocs"]["emails"].append(email)
+            if triage_type == "report":
+                return_dict["signatures"] = self._cleanup_triage_signatures(response.json().get("signatures", []))
+            else:
+                targets = response.json().get("targets", [])
+                return_dict["iocs"] = {
+                    "ips": [],
+                    "urls": [],
+                    "domains": [],
+                    "emails": [],
+                }
+                for target in targets:
+                    iocs = target.get("iocs", {})
+                    for domain in iocs.get("domains", []):
+                        if domain not in return_dict["iocs"]["domains"] and not "in-addr.arpa" in domain:
+                            return_dict["iocs"]["domains"].append(domain) 
+                    for ip in iocs.get("ips", []):
+                        if ip not in return_dict["iocs"]["ips"]:
+                            return_dict["iocs"]["ips"].append(ip)
+                    for url in iocs.get("urls", []):
+                        if url not in return_dict["iocs"]["urls"]:
+                            return_dict["iocs"]["urls"].append(url)
+                    for email in iocs.get("emails", []):
+                        if email not in return_dict["iocs"]["emails"]:
+                            return_dict["iocs"]["emails"].append(email)
 
         return return_dict
+    
+    
+    def _cleanup_triage_signatures(self, signatures:List[dict]) -> List[dict]:
+        """
+        With this method, triage signatures will be cleaned up, especially the indicator tab
+        
+        :param signatures: list of signatures to be cleaned up
+        :type signatures: List[dict]
+        :return: cleaned up signatures
+        """
+        ret_signatures = []
+        
+        for signature in signatures:
+            indicators = signature.get("indicators", [{}])
+            # We generally empty the indicators list ...
+            signature["indicators"] = []
+            # ... but if we have less than 10 indicators, or they are not yara rules, we keep them and process the individual indicators
+            if not ((len(indicators)>10) or "yara_rule" in indicators[0].keys()):
+                for indicator in indicators:
+                    if len(indicator.get("ioc", "")) > 500:
+                        indicator["ioc"] = indicator["ioc"][:500]
+                    signature["indicators"].append(indicator)
+                    
+            ret_signatures.append(signature)  
+        
+        return ret_signatures
     
     
     def _build_hierarchy_from_payload_dict(self, payload_dict:dict) -> dict:
@@ -209,10 +258,12 @@ class KartonPreprocessor(Preprocessor):
         
         for sha256, payload in payload_dict.items():
             if payload.get("payload_type", "") not in ["", "memdump"] and sha256 != root_sha256:
+                if payload.get("payload_type", "") == "config":
+                    self.configs[sha256] = payload
                 hierarchy[sha256] = self._process_payload_entry(payload)
-                if payload.get("payload_parent_id", "") == root_sha256:
+                if payload.get("parent_payload_id", "") == root_sha256:
                     hierarchy["root"]["children"].append(sha256)
-                elif payload.get("payload_parent_id", "") in hierarchy.keys():
+                elif payload.get("parent_payload_id", "") in hierarchy.keys():
                         hierarchy[payload.get("payload_parent_id", "")]["children"].append(sha256)
                 else:
                     self.log.error(f"Failed to find parent payload for payload: {sha256}")
@@ -242,4 +293,24 @@ class KartonPreprocessor(Preprocessor):
             "children": []
         })
 
+        return ret_dict
+    
+    
+    def _process_config_payload(self, config_payload:dict) -> dict:
+        """
+        This method will process a config payload
+        
+        :param config_payload: config payload to be processed
+        :type config_payload: dict
+        :return: processed config payload
+        """
+        
+        ret_dict = {
+            "family": config_payload.get("attributes", {}).get("family", ""),
+            "type": config_payload.get("attributes", {}).get("type", ""),
+            "vetted": config_payload.get("attributes", {}).get("vetted", "false"),
+            "created_by": config_payload.get("created_by", ""),
+            "data": config_payload.get("data", {}),           
+        }
+        
         return ret_dict
