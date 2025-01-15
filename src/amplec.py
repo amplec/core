bbaaecca16647de32a113d8debe36404725bfc40
mo <~>
2025-01-15 10:45:11 +0100

    feat: remimagining this class more as a data-only (not llm) functionality. More changes iminent

--- src/amplec.py
+++ src/amplec.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from utils.logger import Logger
+from utils.simple_persistence import SimplePersistence
 import requests, uuid, os
 from dotenv import load_dotenv
 from preprocessing.karton_preprocessor import KartonPreprocessor
@@ -9,14 +10,14 @@ class Amplec:
     This class is named after the amplec project, as this class will house the most important core functionalities for this project.
     """
     
-    def __init__(self, logger:Logger, global_system_prompt:Optional[str]=None):
+    def __init__(self, logger:Logger, persistence_folder_path:str):
         """
         This is the constructor method for the Amplec class
         
         :param logger: This is a Logger object from the Amplec Utils pip Module.
         :type logger: Logger
-        :param global_system_prompt: IF you set this, it will override the default system prompt, which ONLY will be used if the user does not provide a system prompt himself.
-        :type global_system_prompt: Optional[str]
+        :param persistence_folder_path: This is the folder path where the persistence data will be stored.
+        :type persistence_folder_path: str
         """
         self.log = logger
         
@@ -25,24 +26,36 @@ class Amplec:
         if not self.karton_result_api_url:
             raise ValueError("KARTON_RESULT_API_URL is not set")
         
-        if global_system_prompt:
-            self.system_prompt = global_system_prompt
-        else:
-            self.system_prompt = "You are a helper system for a malware analyst. You will be provided with data from a malware analysis system. Your task is it to answer the questions from the analyst. Do not guess or infer. If some information is not available, just say so."
+        self.persistence = SimplePersistence(persistence_folder_path, logger)
+        
         self.log.info("Amplec initialized")
     
-    def process(self, karton_submission_id:str, regex:Optional[str]=None):
+    def _process_submission_ID(self, karton_submission_id:str, regex:str, repreprocess:Optional[bool]=False) -> list[str]:
         """
         This method will process the submitted data and return the result
+        
+        :param karton_submission_id: This is the submission ID for the karton result
+        :type karton_submission_id: str
+        :param regex: This is an optional regex to filter the data
+        :type regex: str
+        :param repreprocess: This is an optional parameter to reprocess the data, defaults to False
+        :type repreprocess: Optional[bool]
         """
         
-        
-        karton_result = self._retrieve_karton_result(karton_submission_id)
-        self.log.info(f"Succesfully retrieved karton result with ID {karton_submission_id}")
-        
-        pre = KartonPreprocessor(self.log)
-        
-        preprocessed_data = pre.process(karton_result)
+        if not repreprocess:
+            try:
+                preprocessed_data = self.persistence.load_only_payload(karton_submission_id)
+                self.log.info(f"Successfully loaded preprocessed data with ID {karton_submission_id}")
+            except FileNotFoundError:
+                self.log.info(f"Preprocessed data with ID {karton_submission_id} not found, will retrieve it from karton, and process it ourselves.")
+                repreprocess = True
+        if repreprocess:        
+            karton_result = self._retrieve_karton_result(karton_submission_id)
+            self.log.info(f"Succesfully retrieved karton result with ID {karton_submission_id}")
+            
+            pre = KartonPreprocessor(self.log)
+            
+            preprocessed_data = pre.process(karton_result)
         
         return preprocessed_data
         
