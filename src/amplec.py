685ab5c1782c9a7967d89a06853c2a0b9c134c89
mo <~>
2025-01-16 21:23:28 +0100

    feat: change functionality to only process data and search it, no real llm component in the `amplec` class anymore

--- src/amplec.py
+++ src/amplec.py
@@ -1,23 +1,26 @@
 from typing import Optional
 from utils.logger import Logger
 from utils.simple_persistence import SimplePersistence
-import requests, uuid, os
+import requests, uuid, os, re
 from dotenv import load_dotenv
 from preprocessing.karton_preprocessor import KartonPreprocessor
+from preprocessing.nl_preprocessor import NLPreprocessor
+from preprocessing.enricher import Enricher
 
 class Amplec:
     """
     This class is named after the amplec project, as this class will house the most important core functionalities for this project.
+    Which is Preprocessing, Enriching, Naturalizing and Filtering the data. Also the data will be cached to avoid unnecessary reprocessing.
     """
     
-    def __init__(self, logger:Logger, persistence_folder_path:str):
+    def __init__(self, logger:Logger, persistence_folder_path:Optional[str]=None) -> None:
         """
         This is the constructor method for the Amplec class
         
         :param logger: This is a Logger object from the Amplec Utils pip Module.
         :type logger: Logger
-        :param persistence_folder_path: This is the folder path where the persistence data will be stored.
-        :type persistence_folder_path: str
+        :param persistence_folder_path: This is the folder path where the persistence data will be stored. ATTENTION, this is optional, and when not provided the path will be tried to load from the .env file, if neither .env nor parameter is provided, an error will be raised.
+        :type persistence_folder_path: Optional[str]
         """
         self.log = logger
         
@@ -26,11 +29,16 @@ class Amplec:
         if not self.karton_result_api_url:
             raise ValueError("KARTON_RESULT_API_URL is not set")
         
+        if not persistence_folder_path:
+            persistence_folder_path = os.getenv("PERSISTENCE_FOLDER_PATH")
+            if not persistence_folder_path:
+                raise ValueError("PERSISTENCE_FOLDER_PATH is not set, and the variable was also not provided to the constructor")
+        
         self.persistence = SimplePersistence(persistence_folder_path, logger)
         
         self.log.info("Amplec initialized")
         
-    def generate_llm_data_input_from_submission_id(self, karton_submission_id:str, regex_or_search:str, use_regex:Optional[bool], reprocess:Optional[bool]=False) -> list[str]:
+    def generate_llm_data_input_from_submission_id(self, karton_submission_id:str, regex_or_search:str, use_regex:bool, reprocess:Optional[bool]=False) -> list[str]:
         """
         This method will generate the INPUT DATA for the LLM. This method will return a list of strings which underwent the following steps.
         
@@ -51,21 +59,30 @@ class Amplec:
         :param regex_or_search: This is a regex or search string to filter the data
         :type regex_or_search: str
         :param use_regex: This is a boolean to determine if the regex_or_search should be used as a regex or search string
-        :type use_regex: Optional[bool]
+        :type use_regex: bool
         :param reprocess: This is an optional parameter to reprocess the data, defaults to False
         :type reprocess: Optional[bool]
         :return: The generated LLM input data
         """
-        
         
         # first we need to get the enriched data (either from persistence or by processing the karton result)
         enriched_data = self._process_submission_ID(karton_submission_id, reprocess)
         
         search_result = []
         if use_regex:
-            pass #TODO: Implement regex filtering
+            # Use regex to filter the data
+            try:
+                pattern = re.compile(regex_or_search)
+                search_result = [line for line in enriched_data if pattern.search(line)]
+            except re.error as e:
+                raise ValueError(f"Invalid regex pattern: {regex_or_search}") from e
         else:
-            pass #TODO: Implement search string filtering
+            # Use plain search string to filter the data (case-insensitive)
+            for line in enriched_data:
+                lower_line = line.lower()
+                lower_search = regex_or_search.lower()
+                if lower_search in lower_line:
+                    search_result.append(line)
         
         return search_result
         
@@ -76,15 +93,14 @@ class Amplec:
         
         :param karton_submission_id: This is the submission ID for the karton result
         :type karton_submission_id: str
-        :param regex: This is an optional regex to filter the data
-        :type regex: str
         :param repreprocess: This is an optional parameter to reprocess the data, defaults to False
         :type repreprocess: Optional[bool]
+        :return: The data
         """
         
         if not repreprocess:
             try:
-                preprocessed_data = self.persistence.load_only_payload(karton_submission_id)
+                enriched_data = self.persistence.load_only_payload(karton_submission_id)
                 self.log.info(f"Successfully loaded preprocessed data with ID {karton_submission_id}")
             except FileNotFoundError:
                 self.log.info(f"Preprocessed data with ID {karton_submission_id} not found, will retrieve it from karton, and process it ourselves.")
@@ -94,10 +110,17 @@ class Amplec:
             self.log.info(f"Succesfully retrieved karton result with ID {karton_submission_id}")
             
             pre = KartonPreprocessor(self.log)
-            
             preprocessed_data = pre.process(karton_result)
+            
+            nlp = NLPreprocessor(self.log)        
+            naturalized_data = nlp.naturalize(preprocessed_data)
+            
+            enr = Enricher(self.log, "preprocessing/mitre_techniques.json")
+            enriched_data = enr.process(naturalized_data)
+            
+            self.persistence.store_submission(karton_submission_id, enriched_data)
         
-        return preprocessed_data
+        return enriched_data
         
         
     def _retrieve_karton_result(self, submission_id:str) -> dict:
@@ -110,7 +133,7 @@ class Amplec:
         :rtype: dict
         """
         
-        response = requests.get(url=f"{self.karton_result_api_url}submissions/{submission_id}", timeout=30)
+        response = requests.get(url=f"{self.karton_result_api_url}submissions/{submission_id}", timeout=120)
         
         if response.status_code == 200:
             return response.json()
