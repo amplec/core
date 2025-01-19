b7689345a0db6a1b1f8927542e153a9de78fad78
mo <~>
2025-01-19 19:48:07 +0100

    feat!: added naturalization functinality to the other previously unsupported types

--- src/preprocessing/nl_preprocessor.py
+++ src/preprocessing/nl_preprocessor.py
@@ -50,14 +50,13 @@ class NLPreprocessor(Preprocessor):
         
         naturalized_data = []
         try:
-            #naturalized_data.extend(self.naturalize_configs(data["configs"]))
-            #naturalized_data.extend(self.naturalize_hierarchy(data["hierarchy"]))
-            #naturalized_data.extend(self.naturalize_results(data["results"]))
-            for key, value in data["triage_results"].items():
-                naturalized_data.extend(self.naturalize_triage_results(value))
+            naturalized_data.extend(self.naturalize_configs(data["configs"]))
+            naturalized_data.extend(self.naturalize_hierarchy(data["hierarchy"]))
+            naturalized_data.extend(self.naturalize_results(data["results"]))
+            naturalized_data.extend(self.naturalize_triage_results(data["triage_results"]))
         except KeyError as e:
             self.log.error(f"Naturalize could not find the data for the key {e}")
-               
+                
         return naturalized_data
     
     
@@ -70,10 +69,10 @@ class NLPreprocessor(Preprocessor):
         """
         
         naturalized_data = []
-         
-        headline = self._search_for_headline(data)
         
-        naturalized_data.extend(self._recursive_naturalize(data, headline))
+        for key, value in data.items():
+            headline = self._search_for_headline(value)
+            naturalized_data.extend(self._recursive_naturalize(value, headline))
         
         return naturalized_data
     
@@ -217,3 +216,112 @@ class NLPreprocessor(Preprocessor):
                     break
         
         return is_leaf
+    
+    def naturalize_configs(self, data:dict) -> List[str]:
+        """
+        This method will translate a dict of configs (represented as sha256s hashs as strings for keys and the config dicts as values) into a list of natural language equivalent strings.
+        
+        This method expects a dict looking like this:
+        ```json
+        {
+            "<sha256_hash>": {
+                "config": {...}
+            },
+            ...
+        }
+        ```
+        
+        :param data: dict of configs
+        :type data: dict
+        :return: natural language equivalent strings
+        """
+        
+        naturalized_data = []
+        
+        for sha256, config in data.items():
+            headline = f"#sha256 {sha256} is a config "
+            naturalized_data.extend(self._recursive_naturalize(config, headline))
+            
+        return naturalized_data
+        
+    def naturalize_hierarchy(self, data:dict) -> List[str]:
+        """
+        This method will translate a hierarchy dict into a list of natural language equivalent strings.
+        
+        :param data: hierarchy dict
+        :type data: dict
+        :return: natural language equivalent strings
+        """
+        
+        naturalized_data = []
+        
+        root_dict = data.get("root", {})
+        if root_dict:
+            naturalized_data.append(f"The root of the hierarchy is: The {root_dict.get("type", "type not found")} with the sha256 hash {root_dict.get("sha256", "sha256 not found")}")
+            naturalized_data.append(f"The root of the hierarchy has {len(root_dict.get("children", []))} children, {self._count_and_classify_children_for_hierarchy(data, "root")}")
+            
+        for key, value in data.items():
+            if key == "root":
+                continue
+            naturalized_data.append(f"The {value.get("type", "type not found")} in the hierarchy with the sha256 hash {key} has {len(value.get("children", []))} children, {self._count_and_classify_children_for_hierarchy(data, key)}")
+            
+        return naturalized_data
+                
+    def _count_and_classify_children_for_hierarchy(self, data:dict, target:str) -> str:
+        """
+        The `_count_and_classify_children_for_hierarchy` will do exactly what its called. Upon being called, it will examine the chilren of the target entry in the hierarchy.
+        Then it will count the occurences of each type of child and return an ordered string of the types and their counts.
+        
+        :param data: This method expects the full hierarchy dict
+        :type data: dict
+        :param target: The target entry in the hierarchy, for examining the root task set this to "root"
+        :type target: str
+        :return: string containing the ordered types and their counts
+        """
+        
+        if target not in data.keys():
+            self.log.error(f"Could not find the target {target} in the hierarchy")
+            return ""
+        
+        target_dict = data[target]
+        children = target_dict.get("children", [])
+        
+        if not children:
+            self.log.warning(f"The target {target} had no children")
+            return ""
+        
+        type_count = {}
+        for child_key in children:
+            if child_key not in data.keys():
+                self.log.warning(f"Could not find the child {child_key} in the hierarchy")
+                continue
+            child_type = data.get(child_key, {}).get("type", "type not found")
+            type_count[child_type] = 1 + type_count.get(child_type, 0)
+        
+        ordered_types = sorted(type_count.items(), key=lambda x: x[1], reverse=True)
+        ret_str = ""
+        for entry in ordered_types:
+            ret_str += f"{entry[0]}: {entry[1]}, "
+        
+        return ret_str
+    
+    
+    def naturalize_results(self, data:dict) -> List[str]:
+        """
+        This method will be used to naturalize the results
+        
+        :param data: results
+        :type data: dict
+        :return: naturalized results
+        """
+        
+        naturalized_data = []
+        
+        for sha256, list_of_result_dicts in data.items(): #Keep in mind, that the values of this dict, are NOT dicts themselves, but lists of dicts
+            for result_dict in list_of_result_dicts:
+                if result_dict.get("type", "") == "triage":
+                    continue
+                headline = f"#sha256 {sha256} is a result "
+                naturalized_data.extend(self._recursive_naturalize(result_dict, headline))
+        
+        return naturalized_data
